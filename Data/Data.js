8435022075cbdc2723092df12dd225e845f35bad
Fredy Corea <fredycorea@hotmail.com>
2021-03-14 19:47:54 -0600

    Formatting navigation

--- Data/Data.js
+++ Data/Data.js
@@ -1,7 +1,7 @@
 function GetWorkHistoryData(){
 
     var wh =[
-        { "Company": "Costa Cruises",       "From": "01/01/2020", "Until": "01/01/2021", "Title": "Analyst/Programmer", "PagePath": "whCosta.html"  },
+        { "Company": "Global Benefits Group, Inc",       "From": "03/02/2020", "Until": "01/15/2021", "Title": "Analyst/Programmer", "PagePath": "whGbg.html"  },
         { "Company": "Oracle",              "From": "01/01/2020", "Until": "01/01/2021", "Title": "Lead of Development","PagePath": "whOracle.html"},
         { "Company": "Ramstad",             "From": "01/01/2020", "Until": "01/01/2021", "Title": "Lead of Development","PagePath": "whRamstad.html" },
         { "Company": "Zerbone Catering",    "From": "01/01/2020", "Until": "01/01/2021", "Title": "Lead of Development","PagePath": "whZerbone.html" },
