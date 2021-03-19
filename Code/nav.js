94e17bafc3af20471ee09a3b069832bf84c3430d
Fredy Corea <fredycorea@hotmail.com>
2021-03-19 01:03:12 -0600

    Add nav to all pages

--- Code/nav.js
+++ Code/nav.js
@@ -13,9 +13,9 @@ class NavMenu extends HTMLElement {
         <li><a href="Index.html" class="main-menu">Home </a> </li>
         <li><a href="Technologies.html" class="main-menu">Technologies </a> </li>
         <li><a href="WorkHistory.html" class="main-menu">Work History </a> </li>
-        <li><a href="Portfoliio.html" class="main-menu">Portfolio </a> </li>
+        <li><a href="inConstruction.html" class="main-menu">Portfolio </a> </li>
         <li><a href="Education.html" class="main-menu">Education </a></li>
-        <li><a href="References.html" class="main-menu">References </a></li>
+        <li><a href="inConstruction.html" class="main-menu">References </a></li>
     </ul>`;
     }
 
